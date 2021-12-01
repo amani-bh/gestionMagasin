c177bdd30a3a13c92be3e9ccf1c14e5b3fdd10f2
YasmineGrati <yasmine.grati@esprit.tn>
2021-12-01 11:46:50 +0100

    Update StockRestController.java

--- src/main/java/tn/esprit/magasin/controllers/StockRestController.java
+++ src/main/java/tn/esprit/magasin/controllers/StockRestController.java
@@ -3,6 +3,7 @@ package tn.esprit.magasin.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import io.swagger.annotations.ApiOperation;
 import tn.esprit.magasin.entities.Stock;
 import tn.esprit.magasin.services.IStockService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @Api(tags = "Gestion de Stock ")
 @RequestMapping("/stock")
