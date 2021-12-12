90a7af46046373d20ad565deb43432f8c5364ec8
yasminesdr <yasmine.sdiri@esprit.tn>
2021-12-12 21:07:22 +0100

    Update IProduitRepository.java

--- src/main/java/tn/esprit/magasin/repositories/IProduitRepository.java
+++ src/main/java/tn/esprit/magasin/repositories/IProduitRepository.java
@@ -1,9 +1,12 @@
 package tn.esprit.magasin.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import tn.esprit.magasin.entity.Produit;
 
 public interface IProduitRepository extends JpaRepository<Produit, Long>{
+	
+
 
 }
