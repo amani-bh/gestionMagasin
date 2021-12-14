537c0cc9123f02e27294f4bf9e57eb5daab7c139
YasmineGrati <yasmine.grati@esprit.tn>
2021-12-15 00:49:42 +0100

    Update Rayon

--- src/main/java/tn/esprit/magasin/services/IRayonService.java
+++ src/main/java/tn/esprit/magasin/services/IRayonService.java
@@ -2,6 +2,7 @@ package tn.esprit.magasin.services;
 
 import java.util.List;
 
+import tn.esprit.magasin.entity.Produit;
 import tn.esprit.magasin.entity.Rayon;
 
 public interface IRayonService {
@@ -16,4 +17,6 @@ public interface IRayonService {
 	Rayon retrieveRayon(Long id);
 	
 	Rayon RayonByProduit(Long idProduit);
+	
+	List<Produit> RayonByIdProduit(Long idRayon);
 }
